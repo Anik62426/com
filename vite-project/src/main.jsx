d50d2ad7fc8b1827f8e266db1267b46b3db141d3
Aniket Kumar <aniketkumaratl@gmail.com>
2024-03-09 16:12:12 +0530

    defau.t restore

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -5,8 +5,9 @@ import './index.css'
 import ShopContextProvider from './Context/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
     <ShopContextProvider>
     <App />
     </ShopContextProvider> 
-  
+  </React.StrictMode>
 );
